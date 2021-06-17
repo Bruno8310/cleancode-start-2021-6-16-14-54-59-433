b2a795e08b2b0d5192bdd1d9eb78cbc1503ac08b
GONGBR2 <gongbr2@oocl.com>
2021-06-17 19:41:18 +0800

    [$2_magic_number] feature: refactor FareCalculation class

--- src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
+++ src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
@@ -2,12 +2,18 @@ package com.tw.academy.basic.$2_magic_number;
 
 public class FareCalculation {
 
+    private static final Double FIRST_LEVEL_DISCOUNT = 0.8;
+    private static final Double SECOND_LEVEL_DISCOUNT = 0.5;
+    private static final Integer FIRST_MIN_CUMULATIVE_EXPENSES = 100;
+    private static final Integer FIRST_MAN_CUMULATIVE_EXPENSES = 150;
+    private static final Integer RIGHT_MAN_CUMULATIVE_EXPENSES = 400;
+
     public double calculate(double originalPrice, double cumulativeExpensesThisMonth) {
-        if (cumulativeExpensesThisMonth >= 100 && cumulativeExpensesThisMonth < 150) {
-            return originalPrice * 0.8;
+        if (cumulativeExpensesThisMonth >= FIRST_MIN_CUMULATIVE_EXPENSES && cumulativeExpensesThisMonth < FIRST_MAN_CUMULATIVE_EXPENSES) {
+            return originalPrice * FIRST_LEVEL_DISCOUNT;
         }
-        if (cumulativeExpensesThisMonth >= 150 && cumulativeExpensesThisMonth < 400) {
-            return originalPrice * 0.5;
+        if (cumulativeExpensesThisMonth >= FIRST_MAN_CUMULATIVE_EXPENSES && cumulativeExpensesThisMonth < RIGHT_MAN_CUMULATIVE_EXPENSES) {
+            return originalPrice * SECOND_LEVEL_DISCOUNT;
         }
         return originalPrice;
     }
