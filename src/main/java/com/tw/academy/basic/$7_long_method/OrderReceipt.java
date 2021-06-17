82af23944b83c5b6c983485e2419fc6a028de604
GONGBR2 <gongbr2@oocl.com>
2021-06-17 19:21:01 +0800

    [$7_long_method] feature: Extract all constant strings and characters

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -1,7 +1,15 @@
 package com.tw.academy.basic.$7_long_method;
 
 
+import sun.dc.pr.PRError;
+
 public class OrderReceipt {
+    private static final String SALES_TAX = "Sales Tax";
+    private static final String TOTAL_AMOUNT = "Total Amount";
+    private static final String PRINT_INFO_HEAD = "======Printing Orders======";
+    private static final char NEW_LINE = '\n';
+    private static final char TABS= '\t';
+    private static final double DISCOUNT_NUMBER = .10;
     private final Order order;
 
     public OrderReceipt(Order order) {
@@ -10,33 +18,31 @@ public class OrderReceipt {
 
     public String printReceipt() {
         StringBuilder receiptInfos = new StringBuilder();
-
-        receiptInfos.append("======Printing Orders======\n");
-
+        receiptInfos.append(PRINT_INFO_HEAD);
+        receiptInfos.append(NEW_LINE);
         receiptInfos.append(order.getCustomerName());
         receiptInfos.append(order.getCustomerAddress());
-
         double totSalesTx = 0d;
         double tot = 0d;
         for (LineItem lineItem : order.getLineItems()) {
             receiptInfos.append(lineItem.getDescription());
-            receiptInfos.append('\t');
+            receiptInfos.append(TABS);
             receiptInfos.append(lineItem.getPrice());
-            receiptInfos.append('\t');
+            receiptInfos.append(TABS);
             receiptInfos.append(lineItem.getQuantity());
-            receiptInfos.append('\t');
+            receiptInfos.append(TABS);
             receiptInfos.append(lineItem.totalAmount());
-            receiptInfos.append('\n');
+            receiptInfos.append(NEW_LINE);
 
-            double salesTax = lineItem.totalAmount() * .10;
+            double salesTax = lineItem.totalAmount() * DISCOUNT_NUMBER;
             totSalesTx += salesTax;
 
             tot += lineItem.totalAmount() + salesTax;
         }
 
-        receiptInfos.append("Sales Tax").append('\t').append(totSalesTx);
+        receiptInfos.append(SALES_TAX).append(TABS).append(totSalesTx);
 
-        receiptInfos.append("Total Amount").append('\t').append(tot);
+        receiptInfos.append(TOTAL_AMOUNT).append(TABS).append(tot);
         return receiptInfos.toString();
     }
 }
